a1a5c903a1c18cece70f245d89d9559f694b4b40
Ali.Gh <ght.ali@hotmail.com>
2017-12-16 16:23:00 +0100

    init Timer

---
+++ ViewController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Timers;
+using AppKit;
+using Foundation;
+
+namespace MacTimer
+{
+    public partial class ViewController : NSViewController
+    {
+
+        Timer MainTimer;//using system Timer
+        int TimeLeft = 1500; //1500 seconds in 25 minutes
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Fire the timer once a second
+            MainTimer = new Timer(1000);
+            MainTimer.Elapsed += (sender, e) => {
+                TimeLeft--;
+                // Format the remaining time nicely for the label
+                TimeSpan time = TimeSpan.FromSeconds(TimeLeft);
+                string timeString = time.ToString(@"mm\:ss");
+                InvokeOnMainThread(() => {
+                    //We want to interact with the UI from a different thread,
+                    // so we must invoke this change on the main thread
+                    TimerLabel.StringValue = timeString;
+                });
+
+                // If 25 minutes have passed
+                if (TimeLeft == 0)
+                {
+                    // Stop the timer and reset
+                    MainTimer.Stop();
+                    TimeLeft = 1500;
+                    InvokeOnMainThread(() => {
+                        // Reset the UI
+                        TimerLabel.StringValue = "25:00";
+                        StartStopButton.Title = "Start";
+                        NSAlert alert = new NSAlert();
+                        // Set the style and message text
+                        alert.AlertStyle = NSAlertStyle.Informational;
+                        alert.MessageText = "25 Minutes elapsed! Take a 5 minute break.";
+                        // Display the NSAlert from the current view
+                        alert.BeginSheet(View.Window);
+                    });
+                }
+            };
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void StartStopButtonClicked(NSObject sender)
+        {
+            // If the timer is running, we want to stop it,
+            // otherwise we want to start it
+            if (MainTimer.Enabled)
+            {
+                MainTimer.Stop();
+                StartStopButton.Title = "Start";
+            }
+            else
+            {
+                MainTimer.Start();
+                StartStopButton.Title = "Stop";
+            }
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,37 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace MacTimer
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSButton StartStopButton { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField TimerLabel { get; set; }
+
+		[Action ("StartStopButtonClicked:")]
+		partial void StartStopButtonClicked (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (TimerLabel != null) {
+				TimerLabel.Dispose ();
+				TimerLabel = null;
+			}
+
+			if (StartStopButton != null) {
+				StartStopButton.Dispose ();
+				StartStopButton = null;
+			}
+		}
+	}
+}
